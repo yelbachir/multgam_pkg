6bee950d0b4b4911adabc5550e0277156cc6d5bd
Yousra El-Bachir <31545892+yelbachir@users.noreply.github.com>
2020-07-06 22:39:52 +0200

    Delete install.R

--- install.R
+++
@@ -1,8 +0,0 @@
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) ## set working directory to the location of the running file 
-setwd("..")
-
-library(Rcpp)
-
-compileAttributes("multgam")
-
-install.packages("multgam", repos=NULL, type="source")
